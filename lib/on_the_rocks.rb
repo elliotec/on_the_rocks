217100aaa6116c97a5d366349587fab2ce18233c
elliotec <elliotecweb@gmail.com>
2014-09-04 23:02:28 -0600

    write it up

--- lib/on_the_rocks.rb
+++ lib/on_the_rocks.rb
@@ -1,5 +1,46 @@
 require "on_the_rocks/version"
 
 module OnTheRocks
-  # Your code goes here...
+
+  class Installer
+
+    def initialize
+      @sassdir = "app/assets/stylesheets/"
+      @sasspath = "app/assets/stylesheets/application.css.scss"
+      @base = "app/assets/stylesheets/base/_base.scss"
+    end
+
+    def bourbon_install
+      puts `bourbon install` 
+    end
+
+    def truncate
+      puts `mv app/assets/stylesheets/application.css #{@sasspath}` 
+      File.truncate(@sasspath, 0)
+    end
+
+    def neat_install
+      puts `neat install`
+    end
+
+    def bitters_install
+      puts `cd #{@sassdir}`
+      puts `bitters install`
+    end
+
+    def grid_remove
+      grid = Regexp.escape("// @import 'grid_settings';")
+
+      IO.write(@base,
+        File.open(@base) do |file|
+          file.read.gsub(/#{grid}/, "")
+        end
+      )
+    end
+    def write_imports
+      puts `echo -e "@import\\ 'bourbon';\\n@import\\ 'base/grid_settings';\\n
+        @import\\ 'neat';\\n@import\\ 'base/base';\\n\\n//\\ All\\ other\\ imports
+        >>#{@sasspath}"`
+    end
+  end
 end
