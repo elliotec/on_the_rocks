6b037c3e702390c91abe2ab7501930267dd7262e
elliotec <elliotecweb@gmail.com>
2014-09-05 01:33:32 -0600

    refactor to make usable

--- lib/on_the_rocks.rb
+++ lib/on_the_rocks.rb
@@ -1,4 +1,6 @@
-#require "on_the_rocks/version"
+require "on_the_rocks/version"
+require "open-uri"
+require "fileutils"
 
 module OnTheRocks
 
@@ -6,29 +8,31 @@ module OnTheRocks
 
     def initialize
       @sassdir = "app/assets/stylesheets/"
+      @oldcss= "app/assets/stylesheets/application.css"
       @sasspath = "app/assets/stylesheets/application.css.scss"
       @base = "app/assets/stylesheets/base/_base.scss"
     end
 
     def sassify
-      `mv app/assets/stylesheets/application.css #{@sasspath}` 
+      if File.exist?('#{@oldcss}')
+        `mv #{@oldcss} #{@sasspath}`
+      end
       File.truncate(@sasspath, 0)
     end
 
-    def rm_gridsettings
-      grid = Regexp.escape("// @import 'grid_settings';")
-
-      IO.write(@base,
-        File.open(@base) do |file|
-          file.read.gsub(/#{grid}/, "")
-        end
-      )
+    def neat_bitters
+      `neat install`
+      `cd app/assets/stylesheets/ && bitters install`
     end
-    
+
+    def remove_gridsettings
+      @grid = Regexp.escape("// @import 'grid_settings';")
+
+      IO.write(@base, File.open(@base) { |file| file.read.gsub(/#{@grid}/, "") })
+    end
+
     def write_imports
-      `echo -e "@import\\ 'bourbon';\\n@import\\ 'base/grid_settings';\\n
-      @import\\ 'neat';\\n@import\\ 'base/base';\\n\\n//\\ All\\ other\\ imports
-      >>#{@sasspath}"`
+      `echo "@import 'bourbon';\n@import 'base/grid_settings';\n@import 'neat';\n@import 'base/base';\n\n// All other imports">>#{@sasspath}`
     end
   end
 end
